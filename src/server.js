394e258d99add7e2975bca7c58ba594269da1e27
Oliver <ocben1@gmail.com>
2019-07-07 23:38:28 +1000

    test route params in express

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
+const PORT = 8000;
+
 const app = express();
 
 app.use(bodyParser.json());         //parses the JSON object included along with the post request, 
                                     //adding a body property to the request parameter of whatever the matching route is.
 app.get('/hello', (req, res) => res.send('Hello!'));
+app.get('/hello/:name', (req, res) => res.send(`Hello ${req.params.name}`));
 app.post('/hello', (req, res) => res.send(`Hello ${req.body.name}`));
 
-app.listen(8000, () => console.log('Listening on port 8000'));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
