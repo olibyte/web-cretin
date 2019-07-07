60ff91ff6e363da8c4916ccebd603ee94c0c2afd
Oliver <ocben1@gmail.com>
2019-07-07 23:49:02 +1000

    add upvoting for articles

--- src/server.js
+++ src/server.js
@@ -1,14 +1,28 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
+const articlesInfo = {
+    'learn-react': {
+        upvotes: 0,
+    },
+    'learn-node': {
+        upvotes: 0,
+    },
+    'my-thoughts-on-resumes': {
+        upvotes: 0,
+    },
+}
+
 const PORT = 8000;
 
 const app = express();
 
 app.use(bodyParser.json());         //parses the JSON object included along with the post request, 
                                     //adding a body property to the request parameter of whatever the matching route is.
-app.get('/hello', (req, res) => res.send('Hello!'));
-app.get('/hello/:name', (req, res) => res.send(`Hello ${req.params.name}`));
-app.post('/hello', (req, res) => res.send(`Hello ${req.body.name}`));
+app.post('/api/articles/:name/upvote', (req, res) => {
+    const articleName = req.params.name;
 
+    articlesInfo[articleName].upvotes++;
+    res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes`)
+})
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
