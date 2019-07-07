d61ad1d4563adf10eb90aeb9925307950593714c
Oliver <ocben1@gmail.com>
2019-07-08 02:54:24 +1000

    added mongodb

--- src/server.js
+++ src/server.js
@@ -1,27 +1,30 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import { MongoClient } from 'mongodb';
 
-const articlesInfo = {
-    'learn-react': {
-        upvotes: 0,
-        comments:[],
-    },
-    'learn-node': {
-        upvotes: 0,
-        comments:[],
-    },
-    'my-thoughts-on-resumes': {
-        upvotes: 0,
-        comments:[],
-    },
-}
 
 const PORT = 8000;
 
 const app = express();
+//parses the JSON object included along with the post request,
+//adding a body property to the request parameter of whatever the matching route is.
+app.use(bodyParser.json());
 
-app.use(bodyParser.json());         //parses the JSON object included along with the post request, 
-                                    //adding a body property to the request parameter of whatever the matching route is.
+app.get('/api/articles/:name', async (req, res) => {
+    try {
+        const articleName = req.params.name;
+        const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
+        const db = client.db('my-blog');
+
+        const articleInfo = await db.collection('articles').findOne({ name: articleName });
+        res.status(200).json(articleInfo);
+
+        client.close();
+    } catch (error) {
+        res.status(500).json({message: 'Error connecting to db', error});
+
+    }
+});
 app.post('/api/articles/:name/upvote', (req, res) => {
     const articleName = req.params.name;
 
@@ -29,7 +32,7 @@ app.post('/api/articles/:name/upvote', (req, res) => {
     res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes!`)
 });
 app.post('/api/articles/:name/add-comment', (req, res) => {
-    const { username, text} = req.body;
+    const { username, text } = req.body;
 
     const articleName = req.params.name;
 
