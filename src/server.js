91cd1d593d56787d7dd66af094229f628ad03b19
Oliver <ocben1@gmail.com>
2019-07-07 23:31:18 +1000

    added body-parser, tested express endpoints

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
 
+app.use(bodyParser.json());         //parses the JSON object included along with the post request, 
+                                    //adding a body property to the request parameter of whatever the matching route is.
 app.get('/hello', (req, res) => res.send('Hello!'));
+app.post('/hello', (req, res) => res.send(`Hello ${req.body.name}`));
 
 app.listen(8000, () => console.log('Listening on port 8000'));
