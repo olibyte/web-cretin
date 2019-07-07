4eb5d9986e39651badd85a5caa58d0f03b50eda2
Oliver <ocben1@gmail.com>
2019-07-08 01:01:09 +1000

    add comment functionality

--- src/server.js
+++ src/server.js
@@ -4,12 +4,15 @@ import bodyParser from 'body-parser';
 const articlesInfo = {
     'learn-react': {
         upvotes: 0,
+        comments:[],
     },
     'learn-node': {
         upvotes: 0,
+        comments:[],
     },
     'my-thoughts-on-resumes': {
         upvotes: 0,
+        comments:[],
     },
 }
 
@@ -24,5 +27,14 @@ app.post('/api/articles/:name/upvote', (req, res) => {
 
     articlesInfo[articleName].upvotes++;
     res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes!`)
-})
+});
+app.post('/api/articles/:name/add-comment', (req, res) => {
+    const { username, text} = req.body;
+
+    const articleName = req.params.name;
+
+    articlesInfo[articleName].comments.push({ username, text });
+
+    res.status(200).send(articlesInfo[articleName]);
+});
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
